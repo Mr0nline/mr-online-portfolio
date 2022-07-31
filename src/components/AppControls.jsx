f6088e43b96085d38b460c192f379712a260d3a7
Mr.Online <yasanghani7446@gmail.com>
2022-07-31 20:46:17 +0530

    chore: misc file creation

---
+++ src/components/AppControls.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+function AppControls(props) {
+  return (
+    <div className="AppControls">
+      <h1>Ji</h1>
+    </div>
+  );
+}
+
+export default AppControls;
